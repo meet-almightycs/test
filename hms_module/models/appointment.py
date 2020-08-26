d586a1dbb931eb0ff92dd9433b3f0eec05f1d89f
Meet Patel <meetbmpatel@gmail.com>
2020-08-26 20:45:43 +0530

    createorm overright

--- hms_module/models/appointment.py
+++ hms_module/models/appointment.py
@@ -1,27 +1,45 @@
  #-*- coding: utf-8 -*-
 
 from odoo import models, fields, api,_ 
+from datetime import date
 
 
 
 class Hmsappointment(models.Model):
-    _name = 'hms.appointment'
+    _name = 'hms.appointment' 
     _description = "hmsappointment"
     
     identification_no = fields.Char(string='Identification No' ,required = True)
     patient = fields.Many2one('hms.patient' , string = "Patient" , required=True)
     age = fields.Char(string = 'Age' , required = True)
     physician = fields.Many2one('hms.doctor' , string = "Physician")
-    date = fields.Datetime (string = 'Date' , required = True)
+    date = fields.Datetime (string = 'Date')
     urgency_level = fields.Selection([
-    				('normal','Normal'),
-    				('urgent','Urgent'),
-    				('medical emergency','Medical Emergency'),
-    				], required = True , string = 'Urgency Level')
+                    ('normal','Normal'),
+                    ('urgent','Urgent'),
+                    ('medical emergency','Medical Emergency'),
+                    ], required = True , string = 'Urgency Level')
     purpose = fields.Char(string = 'Purpose')
     cabin = fields.Char(string = 'Cabin')
     chief_complaints = fields.Text(string = 'chief Complaints')
     responsible_jr_doctor = fields.Many2one('hms.doctor',string = 'Responsible jr. Doctor')
     history_of_present_illness = fields.Text(string = 'History Of Present Illness')
     outside_appointment = fields.Boolean (string = 'Outside Appointment')
+    state = fields.Selection([
+            ('waiting','Waiting'),
+            ('conform','Conform'),
+
+        ], string = "State")
+
+    @api.model
+    def create(self, vals):
+        #date = vals.get('date', '')
+        # if tag_name:
+        today = fields.Datetime.now()
+       #today = date.today()
+        print("Today's date:", today)
+        vals['date'] = today
+        record =  super(Hmsappointment, self).create(vals)
+        return record
+
 
